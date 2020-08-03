a14efbbb83db29b697ca8967928978621220fba6
Broddiccus <Brody_man02@hotmail.com>
2020-08-03 17:27:35 -0400

    Weapon Mechanics
    added the lifting gauntlet and the bombs into the game, in a basic sandbox for testing.

---
+++ Reminisce/Assets/Scripts/BombScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Bolt;
+
+public class BombScript : MonoBehaviour
+{
+    public SpriteRenderer sprite;
+    Color[] bomb = { new Color(1, 1, 1), new Color(1, 0, 0) };
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Boomtime");
+    }
+    IEnumerator Boomtime()
+    {
+        bool colour = false;
+        float increment = 0.15f;
+        for (float timer = 1; timer > 0; timer -= increment)
+        {
+            colour = !colour;
+            sprite.color = bomb[System.Convert.ToInt32(colour)];
+            yield return new WaitForSeconds(timer);
+        }
+        BOOM();
+        yield return null;
+    }
+    public void BOOM()
+    {
+        gameObject.GetComponent<Collider2D>().enabled = true;
+        //explosion goes here
+        StopCoroutine("Boomtime");
+        Debug.Log(Convert.ToBoolean(Variables.Object(GameObject.Find("Player")).Get("HasBlock")));
+        if (!Convert.ToBoolean(Variables.Object(GameObject.Find("Player")).Get("HasBlock")))
+        {
+            Variables.Object(GameObject.Find("Player")).Set("CanBomb", true);
+        }
+        
+        Destroy(gameObject);
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("HIT");
+            //apply damage
+        }
+    }
+}
---
+++ Reminisce/Assets/Scripts/GrabCollider.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabCollider : MonoBehaviour
+{
+    [SerializeField] Transform[] Directions;
+    public GameObject Block;
+    public GameObject CarriedBlock;
+    public bool canGrab = false;
+    public void ChangeDirections(Vector2 dir)
+    {
+        if (dir[1] == 1)
+        {
+            gameObject.transform.position = Directions[0].position;
+        }
+        if (dir[1] == -1)
+        {
+            gameObject.transform.position = Directions[1].position;
+        }
+        if (dir[0] == 1)
+        {
+            gameObject.transform.position = Directions[2].position;
+        }
+        if (dir[0] == -1)
+        {
+            gameObject.transform.position = Directions[3].position;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("butts");
+        if (other.tag == "Block")
+        {
+            
+            canGrab = true;
+            Block = other.gameObject;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject == Block)
+        {
+            canGrab = false;
+        }
+    }
+    public void GrabTime(bool x)
+    {
+        if (x)
+        {
+            Block.GetComponent<Blocks>().OrderChange(x);
+        }
+        else
+        {
+            CarriedBlock.GetComponent<Blocks>().OrderChange(x);
+        }
+    }
+
+}
